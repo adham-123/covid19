f312b27f10352a90e6bb6e030d320b876e1b19dc
adham-123 <52285471+adham-123@users.noreply.github.com>
2020-12-09 19:05:19 +0200

    deleting some of done commented tasks to-do in index

--- src/index.js
+++ src/index.js
@@ -10,12 +10,9 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// Click on title reset the app
-//search country sticky
-// Graph on hover show not only cases, But also recoverd and deathsnpm (u have to save deaths and recovered with the array in cache so u can have access)
+
 // After user zoom out, if different country selected from dropdown, map doesn't zoom back to it. Check the setZoom on country change
 // When a country is selected table scrolls to that country and highlights raw
-// Title style "Covid 19 Tracker"
 // Check out why the map circles load twice when deaths selected. Maybe cuz "cases" is set to default
 
 // total button not centered on apple
