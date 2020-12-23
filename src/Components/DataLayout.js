52ae7f49b0e0c04486fa6bdcfaef977717d0b238
adham-123 <52285471+adham-123@users.noreply.github.com>
2020-12-23 15:40:06 +0200

    corrected billion to "B" from "G" in formatting

--- src/Components/DataLayout.js
+++ src/Components/DataLayout.js
@@ -11,7 +11,7 @@ function nFormatter(num) {
     { value: 1e18, symbol: "E" },
     { value: 1e15, symbol: "P" },
     { value: 1e12, symbol: "T" },
-    { value: 1e9, symbol: "G" },
+    { value: 1e9, symbol: "B" },
     { value: 1e6, symbol: "M" },
     { value: 1e3, symbol: "k" },
     { value: 1, symbol: "" },
