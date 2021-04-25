3b4f590f4c5bce87a5300e916ab60381e446d826
adham-123 <52285471+adham-123@users.noreply.github.com>
2021-04-25 15:10:58 +0300

    add middleware

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,25 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  combineReducers,
+  getDefaultMiddleware,
+} from "@reduxjs/toolkit";
 import conrenderReducer from "./ducks/conRender";
+import logger from "redux-logger";
 
 const reducer = combineReducers({
   conRender: conrenderReducer,
 });
 
+const firstMiddleware = (store) => (next) => (action) => {
+  return next(action);
+};
+
+const middleware = (getDefaultMiddleware) =>
+  getDefaultMiddleware().concat(firstMiddleware, logger);
+
 const store = configureStore({
   reducer,
+  middleware,
 });
 
 export default store;
