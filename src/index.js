c86c7acf7399363d230fec2580d6298349cb628e
adham-123 <52285471+adham-123@users.noreply.github.com>
2021-05-15 15:58:25 +0300

    Persist added

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import App from "./App";
 import "./index.css";
 import store from "./redux/configureStore";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -5,6 +5,8 @@ import logger from "redux-logger";
 import worldwide from "./reducers/worldwide";
 import selectedCountry from "./reducers/selectedCountry";
 import graphData from "./reducers/graphData";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
 
 const reducer = combineReducers({
   conRender: conrenderReducer,
@@ -14,6 +16,13 @@ const reducer = combineReducers({
   graphData: graphData,
 });
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 const firstMiddleware = (store) => (next) => (action) => {
   return next(action);
 };
@@ -22,8 +31,9 @@ const middleware = (getDefaultMiddleware) =>
   getDefaultMiddleware().concat(firstMiddleware);
 
 const store = configureStore({
-  reducer,
-  middleware,
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: middleware,
 });
 
 export default store;
